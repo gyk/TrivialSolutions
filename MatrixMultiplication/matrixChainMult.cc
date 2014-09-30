5356110c1c490c338386a1deb46b0727a9880a71
Yukun Guo <gyk.net@gmail.com>
2014-09-30 10:56:12 +0800

    More compact input

--- MatrixMultiplication/matrixChainMult.cc
+++ MatrixMultiplication/matrixChainMult.cc
@@ -8,8 +8,6 @@ Matrix-chain Multiplication Problem
 #include <vector>
 #include <iostream>
 
-typedef int pairs[][2];
-
 const int INT_MAX = ((unsigned)(~0)) >> 1;
 
 template <typename T>
@@ -28,16 +26,18 @@ inline int multimes(int r1, int c1r2, int c2)
 	return r1 * c1r2 * c2;
 }
 
-int matrixChainMult(pairs mSizes, int nPairs)
+int matrixChainMult(int* mSizes, int nPairs)
 {
 	auto m = alloc2d<int>(nPairs, nPairs, 0);
+	auto rows = mSizes;
+	auto cols = mSizes + 1;
 
 	for (int len=1; len<nPairs; len++) {
 		for (int i=0; i<nPairs-len; i++) {
 			int min = INT_MAX;
 			for (int pivot=i+1; pivot<=i+len; pivot++) {
 				int nTimes = m[i][pivot-1] + m[pivot][i+len] + 
-					multimes(mSizes[i][0], mSizes[pivot-1][1], mSizes[i+len][1]);
+					multimes(rows[i], cols[pivot-1], cols[i+len]);
 				if (nTimes < min) {
 					min = nTimes;
 				}
@@ -55,9 +55,6 @@ int main(int argc, char const *argv[])
 {
 	std::vector<int> a;
 	for (int v; std::cin >> v; a.push_back(v));
-	int nPairs = a.size() / 2;
-	assert(a.size() - nPairs * 2 == 0);
-	// WORKAROUND: ISO C++ forbids casting to an array type
-	std::cout << matrixChainMult((pairs&)(*&a[0]), nPairs);
+	std::cout << matrixChainMult(&a[0], a.size() - 1);
 	return 0;
 }
