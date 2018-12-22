c1147927505538bc74f8c16687773ca1e9e58b53
Yukun Guo <gyk.net@gmail.com>
2018-12-22 22:48:37 +0800

    Improve implementation

--- MinStack/MinStack.cs
+++ MinStack/MinStack.cs
@@ -2,73 +2,39 @@ using System.Collections.Generic;
 
 public class MinStack
 {
-    List<int> proxy;
-    Stack<int> minIndexSoFar;
-    int currMin;
+    Stack<int> s;
+    Stack<int> minStack;
 
     public MinStack()
     {
-        proxy = new List<int>();
-        minIndexSoFar = new Stack<int>();
-
-        proxy.Add(int.MaxValue);
-        minIndexSoFar.Push(0);
+        s = new Stack<int>();
+        minStack = new Stack<int>();
     }
 
     public void Push(int x)
     {
-        proxy.Add(x);
+        s.Push(x);
 
-        int currMinIndex = minIndexSoFar.Peek();
-        int currMin = proxy[currMinIndex];
-        if (x < currMin) {
-            minIndexSoFar.Push(currentIndex);
-        } else {
-            minIndexSoFar.Push(currMinIndex);
+        if (minStack.Count == 0 || x <= minStack.Peek()) {
+            minStack.Push(x);
         }
     }
 
     public void Pop()
     {
-        if (isEmpty) {
-            throw new System.InvalidOperationException();
+        if (minStack.Peek() == s.Peek()) {
+            minStack.Pop();
         }
-
-        proxy.RemoveAt(currentIndex);
-        minIndexSoFar.Pop();
+        s.Pop();
     }
 
     public int Top()
     {
-        if (isEmpty) {
-            throw new System.InvalidOperationException();
-        }
-
-        return proxy[currentIndex];
+        return s.Peek();
     }
 
     public int GetMin()
     {
-        if (isEmpty) {
-            throw new System.InvalidOperationException();
-        }
-
-        return proxy[minIndexSoFar.Peek()];
-    }
-
-    public bool isEmpty
-    {
-        get
-        {
-            return proxy.Count == 1;
-        }
-    }
-
-    private int currentIndex
-    {
-        get
-        {
-            return proxy.Count - 1;
-        }
+        return minStack.Peek();
     }
 }
