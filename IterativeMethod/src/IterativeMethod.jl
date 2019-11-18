3a1468aec98ae1330cbb1761f69876a4c91e90bd
Yukun Guo <gyk.net@gmail.com>
2019-11-18 13:50:51 +0800

    IterativeMethod: Gauss–Seidel

--- IterativeMethod/src/IterativeMethod.jl
+++ IterativeMethod/src/IterativeMethod.jl
@@ -4,7 +4,8 @@ using LinearAlgebra
 
 export
     is_diagonally_dominant,
-    jacobi_method
+    jacobi_method,
+    gauss_seidel_method, gauss_seidel_method!
 
 #===== Jacobi method =====#
 
@@ -39,8 +40,10 @@ function jacobi_method(
     b::Vector{T},
     x0::Vector{T} = zero(b),
 )::Vector{T} where T<:AbstractFloat
+    (nr, nc) = size(A)
+    @assert nr == nc "Jacobi: A is not square."
     if !is_diagonally_dominant(A)
-        println("Jacobi: the input matrix is not diagonally dominant")
+        println("Jacobi: A is not diagonally dominant")
     end
 
     D = Diagonal(A)
@@ -53,6 +56,68 @@ function jacobi_method(
             return x
         end
         x0 = x
+    end
+    x
+end
+
+#===== Gauss–Seidel method =====#
+
+#=
+
+In Jacobi method,
+
+    x_i^{(k + 1)} = -1/a_ii * ((L + U)_{i, :} * x^{(k)}) + b_i / a_ii
+
+where $U$ ($L$) is the strictly upper (lower) triangular and $R = L + U$. If in the iteration we
+use the elements of $x^{(k+1)}$ that have already been computed to compute the unknown ones of
+$x^{(k+1)}$,
+
+    x_i^{(k + 1)} = -1/a_ii * ((L_*)_{i, :} * x^{(k + 1)} + U_{i, :} * x^{(k)}) + b_i / a_ii
+
+where $L_* = D + L$ is a lower triangular matrix. Or more compcatly,
+
+    A x = b
+    (L_* + U) x = b
+    L_* x = b - U x
+
+    ⟹
+
+    L_* x^{(k + 1)} = b - U x^{(k)}
+    x^{(k + 1)} = L_*^{-1} (b - U x^{(k)})
+
+=#
+
+function gauss_seidel_method(A::Matrix{T}, b::Vector{T})::Vector{T} where T<:AbstractFloat
+    x0 = zero(b)
+    gauss_seidel_method!(A, b, x0)
+end
+
+# The initial guess `x0` will be updated in-place.
+function gauss_seidel_method!(
+    A::Matrix{T},
+    b::Vector{T},
+    x0::Vector{T},
+)::Vector{T} where T<:AbstractFloat
+    (nr, nc) = size(A)
+    @assert nr == nc "Gauss–Seidel: A is not square."
+    n = nr
+    if !is_diagonally_dominant(A)
+        println("Gauss–Seidel: A is not diagonally dominant")
+    end
+
+    x = x0
+    for _ in 1:MAX_ITERATIONS
+        updated = false
+        for r in 1:n
+            old = x[r]
+            x[r] = (b[r] - ((@view A[r, :])' * x - A[r, r] * x[r])) / A[r, r]
+            if !(x[r] ≈ old)
+                updated = true
+            end
+        end
+        if !updated
+            return x
+        end
     end
     x
 end
--- IterativeMethod/test/runtests.jl
+++ IterativeMethod/test/runtests.jl
@@ -14,3 +14,15 @@ using IterativeMethod
         @test A * x ≈ b
     end
 end
+
+@testset "Gauss–Seidel method" begin
+    for i in 1:5
+        n = rand(1:25)
+
+        # Generates a random diagonally dominant matrix
+        A = rand(Float64, n, n) + I(n) * n
+        b = rand(Float64, n)
+        x = gauss_seidel_method(A, b)
+        @test A * x ≈ b
+    end
+end
