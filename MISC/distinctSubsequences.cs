8da40c254e83385ec75d8ca01f4acd86e1aa69d4
Yukun Guo <gyk.net@gmail.com>
2020-07-16 23:31:16 +0800

    distinctSubsequences: improve

--- MISC/distinctSubsequences.cs
+++ MISC/distinctSubsequences.cs
@@ -2,22 +2,16 @@
 
 public class Solution {
     public int NumDistinct(string s, string t) {
-        // appends virtual '^'/'$' at the start/end of both strings
-        var m = new int[s.Length + 2, 2];
-        m[s.Length + 1, (t.Length + 1) % 2] = 1;
+        // appends virtual '$' at the end of both strings
+        var m = new int[s.Length + 1, 2];
+        for (int i = 0; i <= s.Length; i++) {
+            m[i, t.Length % 2] = 1;
+        }
 
-        for (int j = t.Length; j >= 0; j--) {
-            for (int i = s.Length; i >= 0; i--) {
+        for (int j = t.Length - 1; j >= 0; j--) {
+            for (int i = s.Length - 1; i >= 0; i--) {
                 ref var acc = ref m[i + 1, (j + 1) % 2];
-                m[i, (j + 1) % 2] += acc;
-                if ((i, j) switch {
-                    (0, 0) => true,
-                    (_, 0) => false,
-                    (0, _) => false,
-                    (_, _) => (s[i - 1] == t[j - 1]),
-                }) {
-                    m[i, j % 2] = acc;
-                }
+                m[i, j % 2] = s[i] == t[j] ? m[i + 1, j % 2] + acc : m[i + 1, j % 2];
                 acc = 0;
             }
         }
