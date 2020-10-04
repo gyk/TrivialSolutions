24f39a9f060a9a9903cf7bfe055fce471a87b500
Yukun Guo <gyk.net@gmail.com>
2020-10-04 10:59:06 +0800

    Coin Change 2

--- MISC/coinCharge.cs
+++ MISC/coinCharge.cs
@@ -1,6 +1,6 @@
 // Coin Change (https://leetcode.com/problems/coin-change/)
 
-public class Solution {
+public class Solution1 {
     public int CoinChange(int[] coins, int amount) {
         var a = new int?[amount + 1];
         a[0] = 0;
@@ -18,7 +18,7 @@ public class Solution {
     }
 }
 
-public class Solution_Add {
+public class Solution1_Add {
     public int CoinChange(int[] coins, int amount) {
         var a = new int?[amount + 1];
         a[0] = 0;
@@ -34,5 +34,44 @@ public class Solution_Add {
             }
         }
         return a[amount] ?? -1;
+    }
+}
+
+/********************************/
+
+// Coin Change 2 (https://leetcode.com/problems/coin-change-2/)
+
+public class Solution2 {
+    public int Change(int amount, int[] coins) {
+        var a = new int[amount + 1];
+        a[0] = 1;
+        foreach (int c in coins) {
+            for (int j = c; j <= amount; j++) {
+                a[j] += a[j - c];
+            }
+        }
+        return a[amount];
+    }
+}
+
+// My first attempt
+public class Solution2_Stupid {
+    public int Change(int amount, int[] coins) {
+        var a = new int[amount + 1];
+        a[0] = 1;
+        var b = new int[amount + 1];
+        foreach (int c in coins) {
+            a.CopyTo(b, 0);
+            for (int i = 0; i < amount; i++) {
+                if (a[i] == 0) {
+                    continue;
+                }
+                for (int j = i + c; j <= amount; j += c) {
+                    b[j] += + a[i];
+                }
+            }
+            (a, b) = (b, a);
+        }
+        return a[amount];
     }
 }
